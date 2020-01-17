4fb03300f508d1f4a3dba3042377703bdab8e976
Marco Edenhofer <edenhofermarco@t-online.de>
2020-01-17 16:27:57 +0100

    FixedDepositAccounts now get interest and can only be deleted after a certain time

--- src/main/resources/static/js/create_account.js
+++ src/main/resources/static/js/create_account.js
@@ -1,4 +1,4 @@
-let businessDetails = document.getElementById("business-details");
+document.getElementById("business-details");
 let businessForm = document.getElementById("business-customer-form-row");
 let privateForm = document.getElementById("private-customer-form-row");
 let privateCustomerButton = document.getElementById("radioCustomer");
@@ -13,12 +13,12 @@ function toggleBusinessDetails() {
     if (privateCustomerButton.checked) {
         businessForm.classList.add("d-none");
         privateForm.classList.remove("d-none");
-        privateCustomerButton.parentElement.classList.add("active")
+        privateCustomerButton.parentElement.classList.add("active");
         businessCustomerButton.parentElement.classList.remove("active")
     } else {
         businessForm.classList.remove("d-none");
         privateForm.classList.add("d-none");
-        privateCustomerButton.parentElement.classList.remove("active")
+        privateCustomerButton.parentElement.classList.remove("active");
         businessCustomerButton.parentElement.classList.add("active")
     }
 }
